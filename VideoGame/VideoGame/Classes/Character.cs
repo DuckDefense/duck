30bc77af6dddae63f131dc56944e176332ec2f52
ruudschouten <zzding@live.nl>
2015-11-13 14:37:26 +0100

    Added empty Update method

--- VideoGame/VideoGame/Classes/Character.cs
+++ VideoGame/VideoGame/Classes/Character.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace VideoGame.Classes {
     class Character {
         public float Interval { get; set; } // Interval at which the animation should update
-        private float Timer = { get; set; } // Timer that keeps getting updated until the Interval is reached
-        
+        private float Timer { get; set; } // Timer that keeps getting updated until the Interval is reached
+
         public Vector2 Position { get; set; } //Position of the character
         public Point SpriteSize { get; set; } //Height and Width of the sprite
         public Point CurrentFrame { get; set; } //Frame that is being drawn by the SourceRectangle
         public Rectangle PositionRectangle { get; set; } //Rectangle used as collision
         public Rectangle SourceRectangle; //Rectangle needed for animating
-        
+
         public Texture2D FrontSprite; //Sprite that is shown when you're fighting against this character
         public Texture2D BackSprite; //Sprite that is shown when you're fighting as this character
         public Texture2D WorldSprite; //Sprite that is shown when you're walking around on the area
-        
+
         public bool Controllable;
         public string Name;
         public int Money;
@@ -36,21 +38,27 @@ namespace VideoGame.Classes {
         /// <param name="world">Sprite that is shown when you're walking around on the area</param>
         /// <param name="position">Position of the character</param>
         /// <param name="controllable">Is this a playable character</param>
-        public Character(string name, int money, Inventory inventory, List<Monster> monsters, 
-        Texture2D front, Texture2D back, Texture3D world, Vector2 position, bool controllable = false) {
+        public Character(string name, int money, Inventory inventory, List<Monster> monsters,
+        Texture2D front, Texture2D back, Texture2D world, Vector2 position, bool controllable = false) {
             Name = name;
             Money = money;
             Inventory = inventory;
             Monsters = monsters;
             Controllable = controllable;
-            FrontSprite = font;
+            FrontSprite = front;
             BackSprite = back;
             WorldSprite = world;
             Position = position;
-            
+
             SpriteSize = new Point(WorldSprite.Width, WorldSprite.Height);
-            PositionRectangle = new Rectangle(Position.X, Position.Y, SpriteSize.Width, SpriteSize.Height);
+            PositionRectangle = new Rectangle((int)Position.X, (int)Position.Y, SpriteSize.X, SpriteSize.Y);
             SourceRectangle = new Rectangle();
+        }
+
+        //TODO: Add animation function from DuckDefense
+        
+        public void Update(GameTime time) {
+            // Add timer here
         }
     }
 }
