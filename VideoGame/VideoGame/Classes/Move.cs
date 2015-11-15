b92084baa777d306d0bfdff75a5f0e56b872bc4e
ruudschouten <zzding@live.nl>
2015-11-15 14:55:32 +0100

    Added Critical Hit Chance calculations Added Uses

--- VideoGame/VideoGame/Classes/Move.cs
+++ VideoGame/VideoGame/Classes/Move.cs
@@ -20,24 +20,27 @@ namespace VideoGame.Classes {
         public int Damage;
         public Kind Kind;
         public int Accuracy;
+        public int Uses;
         public Type Type;
         public StatModifier HitModifier;
         public StatModifier MissModifier;
         private double damageMod = 1;
 
-        public Move(string name, string description, int damage, int accuracy, Kind kind, Type type) {
+        public Move(string name, string description, int damage, int accuracy, int uses, Kind kind, Type type) {
             Name = name;
             Description = description;
             Damage = damage;
             Accuracy = accuracy;
+            Uses = uses;
             Kind = kind;
             Type = type;
         }
-        public Move(string name, string description, int damage, int accuracy, StatModifier hitModifier, StatModifier missModifier, Kind kind, Type type) {
+        public Move(string name, string description, int damage, int accuracy, int uses, StatModifier hitModifier, StatModifier missModifier, Kind kind, Type type) {
             Name = name;
             Description = description;
             Damage = damage;
             Accuracy = accuracy;
+            Uses = uses;
             HitModifier = hitModifier;
             MissModifier = missModifier;
             Kind = kind;
@@ -50,6 +53,7 @@ namespace VideoGame.Classes {
         /// <param name="user">Monster that is using this move</param>
         /// <param name="receiver">Monster that is receiving this move</param>
         public void Execute(Monster user, Monster receiver) {
+            double critMultiplier = 1;
             //Check if types are very effective or not very effective
             GetDamageModifier(receiver);
             //Check if the move hit
@@ -57,15 +61,19 @@ namespace VideoGame.Classes {
             Random rand = new Random();
             //If random number is below chanceToHit the move did hit
             if (rand.Next(0, 100) <= chanceToHit) {
+                //if random number is below the CriticalHitChance, the move did a critical
+                if (rand.Next(0, 100) <= user.CriticalHitChance) { critMultiplier = user.CriticalHitMultiplier; }
+
                 if (Kind == Kind.Physical) {
-                    var phys = Convert.ToInt32((Damage*(user.Stats.Attack/receiver.Stats.Defense))*damageMod);
+                    var phys = Convert.ToInt32(((Damage * (user.Stats.Attack / receiver.Stats.Defense)) * damageMod) * critMultiplier);
                     receiver.Stats.Health -= phys;
                 }
                 else if (Kind == Kind.Special) {
                     var spec =
-                        Convert.ToInt32((Damage*(user.Stats.SpecialAttack/receiver.Stats.SpecialDefense))*damageMod);
+                        Convert.ToInt32(((Damage * (user.Stats.SpecialAttack / receiver.Stats.SpecialDefense)) * damageMod) * critMultiplier);
                     receiver.Stats.Health -= spec;
                 }
+
                 //TODO: Find out if this will return the same stats if the modifier is empty
                 user.Stats = HitModifier.ApplyModifiers(user);
                 receiver.Stats = HitModifier.ApplyModifiers(receiver);
@@ -85,17 +93,17 @@ namespace VideoGame.Classes {
                 damageMod = 1;
                 break;
             case Type.Fight:
-                    if (prim == Type.Normal || secon == Type.Normal) {
-                        damageMod *= 2;
-                    }
+                if (prim == Type.Normal || secon == Type.Normal) {
+                    damageMod *= 2;
+                }
                 break;
             case Type.Fire:
-                if (prim == Type.Water|| secon == Type.Water) {
+                if (prim == Type.Water || secon == Type.Water) {
                     damageMod *= 2;
                 }
                 break;
             case Type.Water:
-                if (prim == Type.Fire|| secon == Type.Fire) {
+                if (prim == Type.Fire || secon == Type.Fire) {
                     damageMod *= 2;
                 }
                 break;
@@ -112,22 +120,22 @@ namespace VideoGame.Classes {
         #region Physical
         public static Move Tackle() {
             return new Move("Tackle", "A fullbody tackle",
-                60, 100, Kind.Physical, Type.Normal);
+                60, 100, 40, Kind.Physical, Type.Normal);
         }
         public static Move Headbutt() {
             return new Move("Headbutt", "A headbutt",
-                70, 100, Kind.Physical, Type.Normal);
+                70, 100, 25, Kind.Physical, Type.Normal);
         }
         public static Move Strangle() {
             return new Move("Strangle", "The monster strangles the foe",
-                40, 100, Kind.Physical, Type.Normal);
+                40, 100, 20, Kind.Physical, Type.Normal);
         }
         #endregion
 
         #region Special
         public static Move Bubble() {
             return new Move("Bubble", "The monster spits bubbles",
-                40, 100, Kind.Special, Type.Water);
+                40, 100, 30, Kind.Special, Type.Water);
         }
 
         #endregion
@@ -135,17 +143,17 @@ namespace VideoGame.Classes {
         #region NonDamage
         public static Move Glare() {
             //TODO: Test if this will keep reducing stats if used more than once
-            var statMod = new StatModifier(0, 0, 0, 0, .9);
+            var statMod = new StatModifier(0, 0, 0, 0, .75);
             return new Move("Glare", "The monster gives a cold glare and slightly lowers opponents speed",
-                0, 70, statMod, statMod, Kind.NonDamage, Type.Normal);
+                0, 70, 25, statMod, statMod, Kind.NonDamage, Type.Normal);
         }
         public static Move Intimidate() {
-            var hit = new StatModifier(0, 0.9, 0, 0, 0);
-            var miss = new StatModifier(0, 0, 0.9, 0, 0);
+            var hit = new StatModifier(0, 0.75, 0, 0, 0);
+            var miss = new StatModifier(0, 0, 0.75, 0, 0);
             return new Move("Intimidate", "The monster shows the opponent just how intimidating it can be," +
                                           "which will slightly lower the opponents attack," +
                                           "if it misses will lower the users defense",
-                0, 75, hit, miss, Kind.NonDamage, Type.Normal);
+                0, 75, 20, hit, miss, Kind.NonDamage, Type.Normal);
         }
         #endregion
         #endregion
