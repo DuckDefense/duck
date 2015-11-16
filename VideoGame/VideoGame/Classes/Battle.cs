be89c2d498817d420020840e4b0072751ca0c988
ruudschouten <zzding@live.nl>
2015-11-16 21:17:21 +0100

    Added Selection Added LoopTurns which should keep looping until the battle is won

--- VideoGame/VideoGame/Classes/Battle.cs
+++ VideoGame/VideoGame/Classes/Battle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace VideoGame.Classes {
     public enum State {
@@ -9,73 +10,93 @@ namespace VideoGame.Classes {
         Loss,
         Ran
     }
-    class Battle {
-        public List<Monster> UserMonsters;
-        public List<Monster> OpponentMonsters = new List<Monster>();
-        public Monster User;
-        public Monster Opponent;
 
+    public enum Selection {
+        Attack,
+        Item,
+        Party,
+        Run
+    }
+
+    class Battle {
+        public Character User;
+        public Character Opponent;
+        public Monster CurrentUserMonster;
+        public Monster CurrentOpponentMonster;
+        public Selection Selection;
         /// <summary>
         /// Battle with a trainer
         /// </summary>
-        /// <param name="userMonsters">First monster in users party</param>
-        /// <param name="opponentMonsters">First monster in opponents party</param>
-        public Battle(List<Monster> userMonsters, List<Monster> opponentMonsters) {
-            User = userMonsters[0];
-            Opponent = opponentMonsters[0];
-            UserMonsters = userMonsters;
-            OpponentMonsters = opponentMonsters;
+        public Battle(Character user, Character opponent) {
+            User = user;
+            Opponent = opponent;
+            CurrentUserMonster = User.Monsters[0];
+            CurrentOpponentMonster = Opponent.Monsters[0];
         }
 
         /// <summary>
         /// Battle with a wild monster
         /// </summary>
-        /// <param name="userMonsters">First monster in party</param>
-        /// <param name="wildMonster"></param>
-        public Battle(List<Monster> userMonsters, Monster wildMonster) {
-            User = userMonsters[0];
-            UserMonsters = userMonsters;
-            Opponent = wildMonster;
+        public Battle(Character user, Monster opponent) {
+            User = user;
+            CurrentUserMonster = User.Monsters[0];
+            CurrentOpponentMonster = opponent;
         }
 
-        #region BattleOptions NotDone
-
-        public void Attack() {
-            //Show all known moves of the monster
-        }
-
-        public void Items() {
-            //Show all items in the inventory
-        }
-
-        public void Party() {
-            //Show all monsters in inventory
-
-            //If monster is switched out
-            User.Stats = User.PreviousStats;
-            //Select new monster here
-            
-            
-            User.PreviousStats = User.Stats;
-        }
-
-        public void Run() {
-            //Run away from the monster, chance to run away is decided if users monsters speed is higher than the opponents speed
-        } 
-        #endregion
-
-        public void StartBattle(Monster user, Monster receiver) {
+        public void Start() {
             //Store stats so the battle won't alter the stats permanently
-            user.PreviousStats = user.Stats;
-            receiver.PreviousStats = receiver.Stats;
-            
+            CurrentUserMonster.PreviousStats = CurrentUserMonster.Stats;
+            CurrentOpponentMonster.PreviousStats = CurrentOpponentMonster.Stats;
+
+            //Get Ability effects here
+            CurrentUserMonster.Ability.GetEffects(CurrentUserMonster, CurrentOpponentMonster);
+            CurrentOpponentMonster.Ability.GetEffects(CurrentOpponentMonster, CurrentUserMonster);
+
+            //Choose action here, wether its an attack, using an item or switching out a monster
+            //TODO: Make this async
+            LoopTurns();
+
 
             //Restore the stats when the battle is over, or when the monster has been switched out
-            user.Stats = user.PreviousStats;
-            receiver.Stats = receiver.PreviousStats;
 
             //Add experience here so the stats will still be updated if the monster levels up
-            
+
+        }
+
+        public void LoopTurns() {
+
+            var userSpeed = CurrentUserMonster.Stats.Speed;
+            var opponentSpeed = CurrentOpponentMonster.Stats.Speed;
+
+            switch (Selection) {
+            case Selection.Attack:
+                Selection = Selection.Attack;
+                //Draw moves here
+                if (userSpeed > opponentSpeed) {
+                    //User goes before opponent
+                }
+                else {
+                    //Opponent goes first
+                }
+                break;
+            case Selection.Item:
+                Selection = Selection.Item;
+                break;
+            case Selection.Party:
+                Selection = Selection.Party;
+                break;
+            case Selection.Run:
+                Selection = Selection.Run;
+                break;
+            }
+        }
+
+        public void Attack(Monster user, Monster opponent, int chosen) {
+            //Execute chosen move here
+            user.Moves[chosen].Execute(user, opponent);
+            //Wait for the move to complete
+            opponent.Moves[chosen].Execute(opponent, user);
+
         }
     }
 }
