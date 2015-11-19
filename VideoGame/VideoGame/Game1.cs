c1bb4007ec710d948a6fe16dd69065b4dae9cabb
ruudschouten <zzding@live.nl>
2015-11-19 15:03:00 +0100

    Added area drawing

--- VideoGame/VideoGame/Game1.cs
+++ VideoGame/VideoGame/Game1.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
 using VideoGame.Classes;
 
 namespace VideoGame {
@@ -11,8 +13,10 @@ namespace VideoGame {
     public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        private Character Player;
-        private TextureLoader TextureLoader = new TextureLoader();
+        private Character player;
+        private ContentLoader _contentLoader = new ContentLoader();
+        private KeyboardState currentKeyboardState, previousKeyboardState;
+        private Camera2D camera;
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -27,7 +31,8 @@ namespace VideoGame {
         /// </summary>
         protected override void Initialize() {
             // TODO: Add your initialization logic here
-             base.Initialize();
+            IsMouseVisible = true;
+            base.Initialize();
         }
 
         /// <summary>
@@ -37,9 +42,21 @@ namespace VideoGame {
         protected override void LoadContent() {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            TextureLoader.LoadTextures(Content);
-            Player = new Character("Pietertje", 5000, new Inventory(), new List<Monster>(), TextureLoader.GronkeyFront, TextureLoader.GronkeyBack, TextureLoader.GronkeyParty, new Vector2(50,50), true);
-           
+            ContentLoader.SetContent(Content);
+            _contentLoader.LoadContent();
+
+            var viewportAdapter = new ScalingViewportAdapter(GraphicsDevice, 800, 480);
+
+            camera = new Camera2D(viewportAdapter) {
+                Zoom = 0.5f,
+                Position = new Vector2(Area.Route1().Map.WidthInPixels / 4f,
+                Area.Route1().Map.HeightInPixels / 4f)
+            };
+
+            player = new Character("Pietertje", 5000, new Inventory(), new List<Monster>(),
+                ContentLoader.GronkeyFront, ContentLoader.GronkeyBack, ContentLoader.Christman, new Vector2(0, 0), camera);
+            player.CurrentArea = Area.Route1();
+            player.CurrentArea.EnteredArea = true;
 
             // TODO: use this.Content to load your game content here
         }
@@ -58,13 +75,13 @@ namespace VideoGame {
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime) {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            Player.Update(gameTime);
+            currentKeyboardState = Keyboard.GetState();
+            player.Update(gameTime, currentKeyboardState, previousKeyboardState);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+
+            previousKeyboardState = currentKeyboardState;
         }
 
         /// <summary>
@@ -73,10 +90,14 @@ namespace VideoGame {
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
 
-            // TODO: Add your drawing code here
-            Player.Draw(spriteBatch);
-            Area.Route1().Draw(spriteBatch);
+            //Draw areas before player and opponents
+            player.CurrentArea.Draw(player.Camera);
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
