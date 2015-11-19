b167a9b87690d8504d10bcd28e1c1a8cf706dd6f
ruudschouten <zzding@live.nl>
2015-11-19 15:02:33 +0100

    Altered Area so it would work with MonoGame.Extended

--- VideoGame/VideoGame/Classes/Area.cs
+++ VideoGame/VideoGame/Classes/Area.cs
@@ -1,56 +1,54 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using TiledSharp;
+using MonoGame.Extended;
+using MonoGame.Extended.Maps.Tiled;
 
 namespace VideoGame.Classes {
     internal class Area {
-        private static Point Levelrange;
-        public string Monsterarea;
-        public List<Monster> Monsters;
-        public TmxMap Map;
 
-        public Area(string monsterarea, Point levelrange, List<Monster> monsters, TmxMap map) {
+        private static Point Levelrange;
+        public string Name;
+        public List<Monster> Monsters;
+        public bool EnteredArea = false;
+        public TiledMap Map;
+        private static Texture2D tileset;
+        static int tileWidth;
+        static int tileHeight;
+
+        public Area(string name, Point levelrange, List<Monster> monsters, TiledMap map) {
             Levelrange = levelrange;
-            Monsterarea = monsterarea;
+            Name = name;
             Monsters = monsters;
             Map = map;
         }
-
-        public void Draw(SpriteBatch batch) {
-            batch.Begin();
-            for (var i = 0; i < Map.Layers[0].Tiles.Count; i++) {
-                int gid = Map.Layers[0].Tiles[i].Gid;
-                //If tile is not empty
-                if (gid != 0) {
-                    int tileFrame = gid - 1;
-                    int row = tileFrame / (TextureLoader.TileSet.Height / Map.TileHeight);
-
-                    float x = (i % Map.Width) * Map.TileWidth;
-                    float y = (float)Math.Floor(i / (double)Map.Width) * Map.TileHeight;
-
-                    Rectangle tilesetRectangle = new Rectangle(Map.TileWidth * tileFrame, Map.TileHeight * row, 32, 32);
-
-                    batch.Draw(TextureLoader.TileSet, new Rectangle((int) x, (int) y, 32, 32), tilesetRectangle, Color.White);
+        
+        public void Draw(Camera2D camera) {
+            {
+                foreach (var layer in Map.Layers) {
+                    layer.Draw(camera);
                 }
             }
-            batch.End();
         }
-
+        
         #region Route1
         public static Area Route1() {
             Random random = new Random();
-            Point levelrange = new Point(4, 6);
-            var map = new TmxMap(@"Content/Maps/Map.tmx");
+            Point levelrange = new Point(3, 8);
+            
+            var map = ContentLoader.Map;
+
             List<Monster> monsters = new List<Monster> {
                 Monster.Armler(random.Next(levelrange.X, levelrange.Y)),
                 Monster.Gronkey(random.Next(levelrange.X, levelrange.Y))
             };
 
-            return new Area("Route1", levelrange, monsters, map);
+            return new Area("Route 1", levelrange, monsters, map);
         }
         #endregion
     }
