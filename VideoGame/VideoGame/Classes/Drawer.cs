c46a7fa8b94f7b65992f48c03e5ff779fd12f49b
ruudschouten <zzding@live.nl>
2015-11-25 09:38:37 +0100

    Added a class to draw stuff

---
+++ VideoGame/VideoGame/Classes/Drawer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace VideoGame.Classes {
+    public static class Drawer {
+
+        public static void DrawMoves(SpriteBatch batch, Character player, Button attackButton) {
+            Rectangle pos = new Rectangle(attackButton.Position.X - ContentLoader.Button.Width, attackButton.Position.Y + ContentLoader.Button.Height, ContentLoader.Button.Width, ContentLoader.Button.Height);
+            foreach (var move in player.Monsters[0].KnownMoves) {
+                var b = new Button(new Rectangle(pos.X += ContentLoader.Button.Width, pos.Y, pos.Width, pos.Height), ContentLoader.Button, $"{move.Name}", ContentLoader.Arial);
+                b.Draw(batch);
+            }
+        }
+    }
+}
