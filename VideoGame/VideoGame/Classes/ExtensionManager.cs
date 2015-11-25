0b29d1ced1c211aa8c9048594c5956e649b492d5
ruudschouten <zzding@live.nl>
2015-11-25 13:27:51 +0100

    Added ExtensionManager with AddMany method for lists

---
+++ VideoGame/VideoGame/Classes/ExtensionManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class ExtensionManager {
+    public static void AddMany<T>(this List<T> list, params T[] elements) {
+        list.AddRange(elements);
+    }
+}
+
