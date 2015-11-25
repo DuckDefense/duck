efea00556412e174fb7f3f9df7e1e81363ba33d7
ruudschouten <zzding@live.nl>
2015-11-25 13:07:10 +0100

    Added method to draw Buttons and Battle

--- VideoGame/VideoGame/Classes/Drawer.cs
+++ VideoGame/VideoGame/Classes/Drawer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -14,6 +15,25 @@ namespace VideoGame.Classes {
                 var b = new Button(new Rectangle(pos.X += ContentLoader.Button.Width, pos.Y, pos.Width, pos.Height), ContentLoader.Button, $"{move.Name}", ContentLoader.Arial);
                 b.Draw(batch);
             }
+        }
+
+        public static void DrawItems(SpriteBatch batch, Character player) { }
+        public static void DrawParty(SpriteBatch batch, Character player) { }
+
+        public static void DrawBattle(SpriteBatch batch, Monster userMon, Monster oppoMon) {
+            Rectangle userMonsterPos = new Rectangle(
+                ContentLoader.GrassyBackground.Width - (int)(userMon.BackSprite.Width * 6.5),
+                ContentLoader.GrassyBackground.Height - userMon.BackSprite.Height,
+                userMon.BackSprite.Width, 
+                userMon.BackSprite.Height);
+            Rectangle opponentMonsterPos = new Rectangle(
+                ContentLoader.GrassyBackground.Width - (int)(oppoMon.FrontSprite.Width * 2), 
+                ContentLoader.GrassyBackground.Height - (int)(oppoMon.FrontSprite.Height * 2),
+                oppoMon.FrontSprite.Width, 
+                oppoMon.FrontSprite.Height);
+
+            batch.Draw(userMon.BackSprite, new Vector2(userMonsterPos.X, userMonsterPos.Y));
+            batch.Draw(oppoMon.FrontSprite, new Vector2(opponentMonsterPos.X, opponentMonsterPos.Y));
         }
     }
 }
