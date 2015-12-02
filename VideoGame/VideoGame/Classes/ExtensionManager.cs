3b052c0909c2258758e5985b7e7f0b3da42f0cdd
Frankbroeks <fbroeks@hetnet.nl>
2015-12-02 11:48:14 +0100

    Added method to move items in list

--- VideoGame/VideoGame/Classes/ExtensionManager.cs
+++ VideoGame/VideoGame/Classes/ExtensionManager.cs
@@ -7,5 +7,13 @@ public static class ExtensionManager {
     public static void AddMany<T>(this List<T> list, params T[] elements) {
         list.AddRange(elements);
     }
+
+    public static void Move<T>(this List<T> list, T movable, int position)
+    {
+        var item = list[position];
+        int pos = list.IndexOf(movable);
+        list[position] = movable;
+        list[pos] = item;
+    }
 }
 
