4d0001b668f3c28c562d850911f52d0666e675c9
ruudschouten <zzding@live.nl>
2015-12-03 09:51:36 +0100

    Cleaned up code formatting a bit Fixed Great Net Id If item amount is at 0 is now gets removed from the list

--- VideoGame/VideoGame/Classes/Item.cs
+++ VideoGame/VideoGame/Classes/Item.cs
@@ -50,67 +50,34 @@ namespace VideoGame.Classes {
             Random random = new Random();
             int capturechance = 0;
 
-            if (monster.Stats.Health < (monster.PreviousStats.Health/100*20))
-            {
-                capturechance += 25;
+            if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 20)) capturechance += 25;
+            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 40)) capturechance += 20;
+            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 60)) capturechance += 15;
+            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 80)) capturechance += 10;
+
+            switch (monster.Ailment) {
+            case Ailment.Sleep: capturechance += 20; break;
+            case Ailment.Poisoned: capturechance += 10; break;
+            case Ailment.Paralyzed: capturechance += 15; break;
+            case Ailment.Burned: capturechance += 10; break;
+            case Ailment.Dazzled: capturechance += 10; break;
+            case Ailment.Frozen: capturechance += 20; break;
+            case Ailment.Frenzied: capturechance += 5; break;
             }
-            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 40))
-            {
-                capturechance += 20;
-            }
-            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 60))
-            {
-                capturechance += 15;
-            }
-            else if (monster.Stats.Health < (monster.PreviousStats.Health / 100 * 80))
-            {
-                capturechance += 10;
-            }
-            switch (monster.Ailment)
-            {
-                case Ailment.Sleep:
-                    capturechance += 20;
-                    break;
-                case Ailment.Poisoned:
-                    capturechance += 10;
-                    break;
-                case Ailment.Paralyzed:
-                    capturechance += 15;
-                    break;
-                case Ailment.Burned:
-                    capturechance += 10;
-                    break;
-                case Ailment.Dazzled:
-                    capturechance += 10;
-                    break;
-                case Ailment.Frozen:
-                    capturechance += 20;
-                    break;
-                case Ailment.Frenzied:
-                    capturechance += 5;
-                    break;
-            }
+
             capturechance += CaptureChance;
             capturechance += (player.Monsters[0].Level - monster.Level);
             capturechance -= monster.CaptureChance;
 
-            if (capturechance < 5)
-            {
-                capturechance = 5;
-            }
+            if (capturechance < 5) capturechance = 5; 
             var dice = random.Next(0, 100);
-            if (dice < capturechance)
-            {
-                if (player.Monsters.Count >= 6)
-                {
-                    player.Box.Add(monster);
-                }
-                else
-                {
-                    player.Monsters.Add(monster);
-                }
+            if (dice < capturechance) {
+                if (player.Monsters.Count >= 6) player.Box.Add(monster); 
+                else player.Monsters.Add(monster);
             }
-            Amount -= 1;
+            Amount--;
+
+            if (Amount < 1) player.Inventory.Captures.Remove(Id); 
         }
 
         public static Capture RottenNet() {
@@ -120,7 +87,7 @@ namespace VideoGame.Classes {
             return new Capture(2, "Regular Net", "Regular net which seems decent at first glance.", ContentLoader.RegularNet, 45, true, 200, 1, 99);
         }
         public static Capture GreatNet() {
-            return new Capture(2, "Great Net", "A big net that will occasionally catch a monster.", ContentLoader.RegularNet, 65, true, 500, 1, 99);
+            return new Capture(3, "Great Net", "A big net that will occasionally catch a monster.", ContentLoader.GreatNet, 65, true, 500, 1, 99);
         }
     }
 
@@ -204,77 +171,71 @@ namespace VideoGame.Classes {
             MaxAmount = maxAmount;
         }
 
-        public void Use(Monster monster) {
-
-            if (Cures != Cure.None)
-            {
-                if (Cures == Cure.All)
-                {
+        public void Use(Monster monster, Character player) {
+            if (Cures != Cure.None) {
+                if (Cures == Cure.All) {
                     monster.Ailment = Ailment.Normal;
                 }
-                if(monster.Ailment == (Ailment) Cures)
-                {
+                if (monster.Ailment == (Ailment)Cures) {
                     monster.Ailment = Ailment.Normal;
                 }
             }
-            monster.Stats.Health += HealAmount;
-            if (monster.Stats.Health < monster.PreviousStats.Health)
-            {
+            if ((monster.Stats.Health + HealAmount) >= monster.MaxHealth) {
+                monster.Stats.Health = monster.MaxHealth;
+            }
+            else if (monster.Stats.Health != monster.MaxHealth)
+                monster.Stats.Health += HealAmount;
+            else {
+                Amount++;
+            }
+            if (monster.Stats.Health < monster.PreviousStats.Health) {
                 monster.Stats.Health = monster.PreviousStats.Health;
             }
-            Amount -= 1;
-            if (Amount <= 0)
-            {
-                
+            Amount--;
+
+            if (Amount < 1) {
+                player.Inventory.Medicine.Remove(Id);
             }
         }
 
-        public static Medicine LeafBandage()
-        {
-            return new Medicine(1,"Leaf Bandage","Bandage that heals 10 HP", ContentLoader.LeafBandage, 10, 50, 1, 99);
+        public static Medicine LeafBandage() {
+            return new Medicine(1, "Leaf Bandage", "Bandage that heals 10 HP", ContentLoader.LeafBandage, 10, 50, 1, 99);
         }
-        public static Medicine MagicStone()
-        {
+        public static Medicine MagicStone() {
             return new Medicine(2, "Magic Stone", "Stone that heals 20 HP", ContentLoader.MagicStone, 50, 500, 1, 99);
         }
 
-        public static Medicine AntiPoison()
-        {
-            return new Medicine(3,"Anti Poison", "Cures poison", ContentLoader.AntiPoison, Cure.Poisoned, 200, 1, 99);
+        public static Medicine AntiPoison() {
+            return new Medicine(3, "Anti Poison", "Cures poison", ContentLoader.AntiPoison, Cure.Poisoned, 200, 1, 99);
         }
 
-        public static Medicine BucketOfWater()
-        {
-            return new Medicine(4,"Bucket Of Water", "Throw the bucket with a high speed at the monster", ContentLoader.BucketOfWater, -20,Cure.Burned,150,1,99);
+        public static Medicine BucketOfWater() {
+            return new Medicine(4, "Bucket Of Water", "Throw the bucket with a high speed at the monster", ContentLoader.BucketOfWater, -20, Cure.Burned, 150, 1, 99);
         }
 
-        public static Medicine Salt()
-        {
-            return new Medicine(5, "Salt", "Throw salt at the frozen monster\n" + " !WARNING if your monster is a snail it WILL die!", ContentLoader.Salt, Cure.Frozen, 150, 1 ,99);
+        public static Medicine Salt() {
+            return new Medicine(5, "Salt", "Throw salt at the frozen monster\n" + " !WARNING if your monster is a snail it WILL die!", ContentLoader.Salt, Cure.Frozen, 150, 1, 99);
         }
 
-        public static Medicine AirHorn()
-        {
-            return new Medicine(6, "Air Horn", "Blow the horn in the ears of the monster", ContentLoader.AirHorn, Cure.Sleep, 200, 1,99);
+        public static Medicine AirHorn() {
+            return new Medicine(6, "Air Horn", "Blow the horn in the ears of the monster", ContentLoader.AirHorn, Cure.Sleep, 200, 1, 99);
         }
 
-        public static Medicine SpinalCord()
-        {
+        public static Medicine SpinalCord() {
             return new Medicine(7, "Spinal Cord", "Perform surgery on the monter to give it a new spinal cord\n" +
                                                   "Even if it didn't have one before.",
-                ContentLoader.SpinalCord ,Cure.Paralyzed, 250, 1, 99);
+                ContentLoader.SpinalCord, Cure.Paralyzed, 250, 1, 99);
         }
 
-        public static Medicine RoosVicee()
-        {
-            return new Medicine(8, "RoosVicee", "Komt wel goed schatje", ContentLoader.RoosVicee, Cure.All, 586, 1,99);
+        public static Medicine RoosVicee() {
+            return new Medicine(8, "RoosVicee", "Komt wel goed schatje", ContentLoader.RoosVicee, Cure.All, 586, 1, 99);
         }
     }
 
     public class KeyItem : Item {
         //Not sure if we need this
 
-        
+
         public void Use() {
             //Use the key!
         }
