f6feddee246d5b4e54291e57a75bc2870fb4d64e
ruudschouten <zzding@live.nl>
2015-12-09 10:28:47 +0100

    Added new Interface for timers Moved Interfaces to a different folder

---
+++ VideoGame/VideoGame/Classes/Interfaces/ITimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace VideoGame.Classes {
+    public interface ITimer {
+        /// <summary>
+        /// Interval at which the animation should update
+        /// </summary>
+        float Interval { get; set; }
+        /// <summary>
+        /// Timer that keeps getting updated until the Interval is reached
+        /// </summary>
+        float Timer { get; set; }
+    }
+}
