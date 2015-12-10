ae599635f5828ee363d8e1d8a0aec7343eb48819
ruudschouten <zzding@live.nl>
2015-12-10 12:26:45 +0100

    Added very slow collision detection, Currently only the first battle will work, needs a fix

--- VideoGame/VideoGame/Classes/Area.cs
+++ VideoGame/VideoGame/Classes/Area.cs
@@ -18,8 +18,13 @@ namespace VideoGame.Classes {
         public bool EnteredArea = false;
         public TiledMap Map;
         private static Texture2D tileset;
-        static int tileWidth;
-        static int tileHeight;
+        private Rectangle encounterHitbox;
+        private Rectangle collisionHitbox;
+        List<Rectangle> EncounterColiders = new List<Rectangle>();
+        List<Rectangle> CollisionColiders = new List<Rectangle>();
+        static int tileWidth = 16;
+        static int tileHeight = 16;
+        private Vector2 previousPos = new Vector2();
 
         public Area(string name, Point levelrange, List<Monster> monsters, TiledMap map) {
             Levelrange = levelrange;
@@ -28,40 +33,73 @@ namespace VideoGame.Classes {
             Map = map;
         }
 
-        public void Draw(Camera2D camera) {
+        public void Draw(Camera2D camera, SpriteBatch batch) {
+            //Map.Draw(camera, true);
             foreach (var layer in Map.Layers) {
                 layer.Draw(camera);
             }
+            //foreach (var encounterColider in EncounterColiders) { batch.Draw(ContentLoader.Button, encounterColider, Color.Green); }
+            //foreach (var collisionColider in CollisionColiders) { batch.Draw(ContentLoader.Button, collisionColider, Color.Blue); }
         }
 
         public void GetCollision(Character player) {
-            foreach (var layer in Map.Layers) {
-                if (layer.Properties.Count != 0) {
-                    var layerPropKeys = layer.Properties.Keys.ToList();
-                    var layerPropValues = layer.Properties.Values.ToList();
-                    foreach (var key in layerPropKeys) {
-                        if (key == "Collision") {
-                            foreach (var value in layerPropValues) {
-                                if (value == "true") {
-                                    //There is collision and player can not move here
-                                }
-                            }
-                        }
-                        if (key == "Encounters") {
-                            foreach (var value in layerPropValues) {
-                                if (value == "true") {
-                                    //When walking on this spot there is a chance to get encounters
-                                    double chanceToEncounter = 75;
-                                    Random random = new Random();
-                                    if (chanceToEncounter >= random.Next(0, 100)) {
-
-                                    }
-                                }
-                            }
-                        }
+            CollisionColiders.Clear();
+            var collisionLayers = Map.TileLayers.Where(layer => layer.Properties.ContainsKey("Collision") && layer.Properties.ContainsValue("true")).ToList();
+            foreach (var layer in collisionLayers) {
+                foreach (var tile in layer.Tiles) {
+                    if (tile.Id != 0) {
+                        collisionHitbox = new Rectangle((tile.X * tileWidth), (tile.Y * tileHeight), tileWidth, tileHeight);
+                        CollisionColiders.Add(collisionHitbox);
                     }
                 }
             }
+            if (CollisionColiders.Count != 0) {
+                foreach (var collision in CollisionColiders) {
+                    if (player.Hitbox.Intersects(collision)) {
+                        var vector = new Vector2(collision.X, collision.Y);
+                        //If right of it
+                        if (vector.X >= player.Position.X) { player.Position.X -= 1; }
+                        //if left of it
+                        if (vector.X <= player.Position.X) { player.Position.X += 1; }
+                        //if above it
+                        if (vector.Y >= player.Position.Y) { player.Position.Y -= 1; }
+                        //if below it
+                        if (vector.Y <= player.Position.Y) { player.Position.Y += 1; }
+                    }
+                }
+            }
+        }
+
+        public void GetEncounters(Character player, ref Battle battle, ref bool battling) {
+            EncounterColiders.Clear();
+            var encounterLayers = Map.TileLayers.Where(layer => layer.Properties.ContainsKey("Encounters") && layer.Properties.ContainsValue("true")).ToList();
+            foreach (var layer in encounterLayers) {
+                foreach (var tile in layer.Tiles) {
+                    if (tile.Id != 0) {
+                        encounterHitbox = new Rectangle((tile.X * tileWidth), (tile.Y * tileHeight), tileWidth, tileHeight);
+                        EncounterColiders.Add(encounterHitbox);
+                    }
+                }
+            }
+            if (EncounterColiders.Count != 0) {
+                foreach (var collision in EncounterColiders) {
+                    if (player.Hitbox.Intersects(collision)) {
+                        Random rand = new Random();
+                        var chanceToEncounter = rand.Next(0, 100);
+                        if (player.Position != previousPos)
+                            if (chanceToEncounter <= 5) {
+                                battling = true;
+                                battle = new Battle(player, GetRandomMonster());
+                            }
+                    }
+                }
+            }
+            previousPos = player.Position;
+        }
+
+        public Monster GetRandomMonster() {
+            Random rand = new Random();
+            return Monsters[rand.Next(0, Monsters.Count)];
         }
 
         #region Route1
