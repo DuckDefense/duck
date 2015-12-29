e7c64df3083c6009da38a941931bd7068c80affd
ruudschouten <zzding@live.nl>
2015-12-29 16:11:10 +0100

    Added rectangles for tilechecking. Altered the AnimateWorld a bit so it'll loop the entire picture

--- VideoGame/VideoGame/Classes/Character.cs
+++ VideoGame/VideoGame/Classes/Character.cs
@@ -9,11 +9,9 @@ using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
 
-namespace VideoGame.Classes
-{
+namespace VideoGame.Classes {
 
-    public enum Direction
-    {
+    public enum Direction {
         None,
         Up,
         Down,
@@ -21,12 +19,12 @@ namespace VideoGame.Classes
         Left
     }
 
-    public class Character : IAnimatable, ITimer
-    {
+    public class Character : IAnimatable, ITimer {
         public float Interval { get; set; } = 200; // Interval at which the animation should update
         public float Timer { get; set; } = 0; // Timer that keeps getting updated until the Interval is reached
-
+        
         public Vector2 Position; //Position of the character
+        public Rectangle LeftTile, RightTile, UpperTile, LowerTile; //Tiles that surround the character
         public Point SpriteSize = new Point(32, 32);//Height and Width of the sprite
         public Point CurrentFrame = new Point(0, 0);//Frame that is being drawn by the SourceRectangle
 
@@ -69,14 +67,12 @@ namespace VideoGame.Classes
         /// <param name="position">Position of the character</param>
         /// <param name="controllable"></param>
         public Character(string name, int money, Inventory inventory, List<Monster> monsters,
-        Texture2D front, Texture2D back, Texture2D world, Vector2 position, bool controllable)
-        {
+        Texture2D front, Texture2D back, Texture2D world, Vector2 position, bool controllable) {
             Name = name;
             Money = money;
             Inventory = inventory;
             Monsters = monsters;
-            foreach (Monster monster in Monsters)
-            {
+            foreach (Monster monster in Monsters) {
                 monster.IsWild = false;
             }
             Controllable = controllable;
@@ -98,14 +94,12 @@ namespace VideoGame.Classes
         /// <param name="world">Sprite that is shown when you're walking around on the area</param>
         /// <param name="position">Position of the character</param>
         public Character(string name, int money, Inventory inventory, List<Monster> monsters,
-        Texture2D front, Texture2D back, Texture2D world, Vector2 position)
-        {
+        Texture2D front, Texture2D back, Texture2D world, Vector2 position) {
             Name = name;
             Money = money;
             Inventory = inventory;
             Monsters = monsters;
-            foreach (Monster monster in Monsters)
-            {
+            foreach (Monster monster in Monsters) {
                 monster.IsWild = false;
             }
             Controllable = false;
@@ -115,23 +109,19 @@ namespace VideoGame.Classes
             Position = position;
         }
 
-        //TODO: Add animation function from DuckDefense
-
-        public void Update(GameTime time, KeyboardState cur, KeyboardState prev)
-        {
-            if (CountingDown)
-            {
+        public void Update(GameTime time, KeyboardState cur, KeyboardState prev) {
+            if (CountingDown) {
                 MovementTimer(time);
             }
-            else
-            {
+            else {
                 GetDirection(cur, prev);
-                
                 Movement(cur, time);
             }
-            SourceRectangle = new Rectangle(CurrentFrame.X*SpriteSize.X, CurrentFrame.Y*SpriteSize.Y, SpriteSize.X, SpriteSize.Y);
-            if (CurrentArea != null) Hitbox = new Rectangle((int) Position.X, (int) Position.Y, 32, 32);    
-            AnimateWorld(time);
+            SourceRectangle = new Rectangle(CurrentFrame.X * SpriteSize.X, CurrentFrame.Y * SpriteSize.Y, SpriteSize.X, SpriteSize.Y);
+            if (CurrentArea != null) Hitbox = new Rectangle((int)Position.X, (int)Position.Y, 32, 32);
+            if (Direction == Direction.None) CurrentFrame.X = 0;
+            else
+                AnimateWorld(time);
         }
 
         public void MonsterUpdate(GameTime time) {
@@ -140,178 +130,132 @@ namespace VideoGame.Classes
             }
         }
 
-        public void Draw(SpriteBatch batch)
-        {
-            if (Debug)
-            {
+        public void Draw(SpriteBatch batch) {
+            if (Debug) {
                 batch.Draw(ContentLoader.Button, null, LineOfSightRectangle, null, null, 0f, Vector2.Zero, Color.White);
                 batch.Draw(ContentLoader.Button, Hitbox, Color.Red);
+                //Tiles
+                batch.Draw(ContentLoader.Button, LeftTile, Color.LemonChiffon);
+                batch.Draw(ContentLoader.Button, RightTile, Color.Red);
+                batch.Draw(ContentLoader.Button, UpperTile, Color.Turquoise);
+                batch.Draw(ContentLoader.Button, LowerTile, Color.BurlyWood);
             }
             batch.Draw(WorldSprite, Position, SourceRectangle, Color.White);
         }
 
-        public void GetDirection(KeyboardState cur, KeyboardState prev)
-        {
-            if (Controllable)
-            {
+        public void GetDirection(KeyboardState cur, KeyboardState prev) {
+            if (Controllable) {
                 Direction = Direction.None;
-                if (cur.IsKeyDown(Settings.moveUp) || cur.IsKeyDown(Keys.Up))
-                {
+                if (cur.IsKeyDown(Settings.moveUp) || cur.IsKeyDown(Keys.Up)) {
                     Direction = Direction.Up;
                     CurrentFrame.Y = 3;
                 }
-                if (cur.IsKeyDown(Settings.moveLeft) || cur.IsKeyDown(Keys.Left))
-                {
+                if (cur.IsKeyDown(Settings.moveLeft) || cur.IsKeyDown(Keys.Left)) {
                     Direction = Direction.Left;
                     CurrentFrame.Y = 2;
                 }
-                if (cur.IsKeyDown(Settings.moveRight) || cur.IsKeyDown(Keys.Right))
-                {
+                if (cur.IsKeyDown(Settings.moveRight) || cur.IsKeyDown(Keys.Right)) {
                     Direction = Direction.Right;
                     CurrentFrame.Y = 1;
                 }
-                if (cur.IsKeyDown(Settings.moveDown) || cur.IsKeyDown(Keys.Down))
-                {
+                if (cur.IsKeyDown(Settings.moveDown) || cur.IsKeyDown(Keys.Down)) {
                     Direction = Direction.Down;
                     CurrentFrame.Y = 0;
                 }
             }
         }
 
-        public void SetLineOfSight(int tiles)
-        {
+        public void SetLineOfSight(int tiles) {
             var size = (tiles + 1) * 16;
-            switch (Direction)
-            {
-                case Direction.None:
-                    break;
-                case Direction.Up:
-                    LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y - (size - (WorldSprite.Height / 3)), (WorldSprite.Width / 3), size);
-                    break;
-                case Direction.Down:
-                    LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y, (WorldSprite.Width / 3), size);
-                    break;
-                case Direction.Right:
-                    LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y, size, (WorldSprite.Height) / 4);
-                    break;
-                case Direction.Left:
-                    LineOfSightRectangle = new Rectangle((int)Position.X - (size - (WorldSprite.Width / 3)), (int)Position.Y, size, (WorldSprite.Height / 4));
-                    break;
+            switch (Direction) {
+            case Direction.None:
+                break;
+            case Direction.Up:
+                LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y - (size - (WorldSprite.Height / 3)), (WorldSprite.Width / 3), size);
+                break;
+            case Direction.Down:
+                LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y, (WorldSprite.Width / 3), size);
+                break;
+            case Direction.Right:
+                LineOfSightRectangle = new Rectangle((int)Position.X, (int)Position.Y, size, (WorldSprite.Height) / 4);
+                break;
+            case Direction.Left:
+                LineOfSightRectangle = new Rectangle((int)Position.X - (size - (WorldSprite.Width / 3)), (int)Position.Y, size, (WorldSprite.Height / 4));
+                break;
             }
         }
-        public void Movement(KeyboardState cur, GameTime time)
-        {
-
-
-            if (Controllable)
-            {
-                if (cur.IsKeyDown(Settings.moveUp) || cur.IsKeyDown(Keys.Up))
-                {
+        public void Movement(KeyboardState cur, GameTime time) {
+            if (Controllable) {
+                if (cur.IsKeyDown(Settings.moveUp) || cur.IsKeyDown(Keys.Up)) {
                     Direction = Direction.Up;
                     MovementTimer(time);
                 }
-                if (cur.IsKeyDown(Settings.moveDown) || cur.IsKeyDown(Keys.Down))
-                {
+                else if (cur.IsKeyDown(Settings.moveDown) || cur.IsKeyDown(Keys.Down)) {
                     Direction = Direction.Down;
                     MovementTimer(time);
 
                 }
-                if (cur.IsKeyDown(Settings.moveLeft) || cur.IsKeyDown(Keys.Left))
-                {
+                else if (cur.IsKeyDown(Settings.moveLeft) || cur.IsKeyDown(Keys.Left)) {
                     Direction = Direction.Left;
                     MovementTimer(time);
                 }
-                if (cur.IsKeyDown(Settings.moveRight) || cur.IsKeyDown(Keys.Right))
-                {
+                else if (cur.IsKeyDown(Settings.moveRight) || cur.IsKeyDown(Keys.Right)) {
                     Direction = Direction.Right;
                     MovementTimer(time);
                 }
             }
-
-            
-            //move here
-            //
-
         }
 
-        public void MovementTimer(GameTime time)
-        {
+        public void MovementTimer(GameTime time) {
             CountingDown = true;
-            float move = (float) (32 * time.ElapsedGameTime.TotalSeconds * 2) ;
+            float move = (float)(32 * time.ElapsedGameTime.TotalSeconds * 4);
             Moved += move;
-            switch (Direction)
-            {
-                case Direction.Up:
-                    Position.Y -= move;
-                    break;
-                case Direction.Down:
-                    Position.Y += move;
-                    break;
-                case Direction.Right:
-                    Position.X += move;
-                    break;
-                case Direction.Left:
-                    Position.X -= move;
-                    break;
+            switch (Direction) {
+            case Direction.Up: Position.Y -= move; break;
+            case Direction.Down: Position.Y += move; break;
+            case Direction.Right: Position.X += move; break;
+            case Direction.Left: Position.X -= move; break;
             }
-            //if (Timer > Interval - 100)
-            if(Moved >= 32)
-            {
+            SetTilePositions();
+            if (Moved >= 32) {
                 CountingDown = false;
-                Direction = Direction.None;
-                Timer = 0f;
                 Moved = 0;
+                //CheckGrid();
             }
         }
 
-        public void MoveToPoint(Vector2 move, float delay)
-        {
+        public bool LeftCollide(Rectangle collision) { return collision.Intersects(LeftTile); }
+        public bool RightCollide(Rectangle collision) { return collision.Intersects(RightTile); }
+        public bool UpperCollide(Rectangle collision) { return collision.Intersects(UpperTile); }
+        public bool LowerCollide(Rectangle collision) { return collision.Intersects(LowerTile); }
 
-            var distanceX = (move.X - Position.X) / delay;
-            var distanceY = (move.Y - Position.Y) / delay;
-            //var moveX = distanceX/delay;
-            //var moveY = distanceY/delay;
-
-            Hitbox = new Rectangle((int)move.X,
-                (int)move.Y,
-                16, 16);
-            if (!Hitbox.Contains(Position))
-            {
-                Position.X += distanceX * 2;
-                Position.Y += distanceY * 2;
-            }
+        private void SetTilePositions() {
+            LeftTile = new Rectangle((int)Position.X - 16, (int)Position.Y + 16, 4, 4);
+            RightTile = new Rectangle((int)Position.X + (16 + SpriteSize.X) - 2, (int)Position.Y + 16, 4, 4);
+            UpperTile = new Rectangle((int)Position.X + SpriteSize.X / 2, (int)Position.Y - (16), 4, 4);
+            LowerTile = new Rectangle((int)Position.X + SpriteSize.X / 2, (int)Position.Y + (16 + SpriteSize.Y), 4, 4);
         }
 
-        public void AnimateWorld(GameTime gametime)
-        {
-            if (Direction == Direction.None) CurrentFrame.X = 0;
-            else
-            {
-                Timer += (float)gametime.ElapsedGameTime.TotalMilliseconds;
-                if (Timer > Interval)
-                {
-                    CurrentFrame.X++;
-                    if (CurrentFrame.X > WorldSprite.Width / SpriteSize.X - 1)
-                    {
-                        CurrentFrame.X = 1;
-                    }
-                    Timer = 0f;
+        public void AnimateWorld(GameTime gametime) {
+            Timer += (float)gametime.ElapsedGameTime.TotalMilliseconds;
+            if (Timer > Interval) {
+                CurrentFrame.X++;
+                if (CurrentFrame.X > WorldSprite.Width / SpriteSize.X - 1) {
+                    CurrentFrame.X = 0;
                 }
+                Timer = 0f;
             }
         }
 
-        public void AnimateFront(GameTime gametime)
-        {
+        public void AnimateFront(GameTime gametime) {
             throw new NotImplementedException();
         }
 
-        public void AnimateBack(GameTime gametime)
-        {
+        public void AnimateBack(GameTime gametime) {
             throw new NotImplementedException();
         }
 
-        public void AnimateParty(GameTime gametime)
-        {
+        public void AnimateParty(GameTime gametime) {
             throw new NotImplementedException();
         }
     }
