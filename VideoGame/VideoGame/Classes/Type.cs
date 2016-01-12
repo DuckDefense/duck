64cbe8b0529f7c862bc40719729b6f9b1f60690f
ruudschouten <zzding@live.nl>
2016-01-12 09:42:12 +0100

    Added a method to get Types from a string

--- VideoGame/VideoGame/Classes/Type.cs
+++ VideoGame/VideoGame/Classes/Type.cs
@@ -19,4 +19,25 @@ namespace VideoGame.Classes {
         Ghost,
         Sound
     }
+
+    public static class TypeMethods {
+        public static Type GetTypeFromString(string s) {
+            switch (s) {
+            case "None": return Type.None;
+            case "Normal": return Type.Normal;
+            case "Fight": return Type.Fight;
+            case "Fire": return Type.Fire;
+            case "Water": return Type.Water;
+            case "Grass": return Type.Grass;
+            case "Rock": return Type.Rock;
+            case "Poison": return Type.Poison;
+            case "Psych": return Type.Psych;
+            case "Flying": return Type.Flying;
+            case "Ice": return Type.Ice;
+            case "Ghost": return Type.Ghost;
+            case "Sound": return Type.Sound;
+            }
+            return Type.None;
+        }
+    }
 }
