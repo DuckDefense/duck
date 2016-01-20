e1e8589ede67d1601ef2720073c1a2043aeb71a8
ruudschouten <zzding@live.nl>
2016-01-20 10:00:14 +0100

    Updated to work with new character class

--- VideoGame/VideoGame/Classes/Shop.cs
+++ VideoGame/VideoGame/Classes/Shop.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Sandbox.Classes;
 
-namespace VideoGame.Classes
-{
-    public class Shop
-    {
+namespace VideoGame.Classes {
+    public class Shop {
         public Character ShopKeeper;
         public string Name;
 
@@ -17,58 +16,46 @@ namespace VideoGame.Classes
 
         private SpriteBatch Batch;
 
-        public Shop(int money, Inventory inventory, string name, SpriteBatch batch)
-        {
+        public Shop(int money, Inventory inventory, string name, SpriteBatch batch) {
             Name = name;
-            ShopKeeper  = new Character("ShopKeeper", money, inventory,new List<Monster>(), null, null, ContentLoader.ChristmanWorld, Vector2.Zero);
+            var introLines = new List<string> { "Yello!\n Feel free to buy some of my stock" };
+            var byeLines = new List<string> { "Thanks for stopping by" };
+            ShopKeeper = new Character("ShopKeeper", money, inventory, introLines, byeLines,
+                ContentLoader.ChristmanFront, ContentLoader.ChristmanBack, ContentLoader.ChristmanWorld,
+                Vector2.Zero);
             Batch = batch;
         }
 
-        public void DrawInventory()
-        {
+        public void DrawInventory() {
             Drawer.DrawInventory(Batch, ShopKeeper);
         }
-        public void Buy()
-        {
-            if (Drawer.DrawMedicine)
-            {
-                foreach (var m in ShopKeeper.Inventory.Medicine)
-                {
-                    if (m.Value.Name == Drawer.GetClickedButton().Text)
-                    {
+        public void Buy() {
+            if (Drawer.DrawMedicine) {
+                foreach (var m in ShopKeeper.Inventory.Medicine) {
+                    if (m.Value.Name == Drawer.GetClickedButton().Text) {
                         SelectedMedicine = m.Value;
                     }
                 }
             }
-            if (Drawer.DrawCapture)
-            {
-                foreach (var m in ShopKeeper.Inventory.Captures)
-                {
-                    if (m.Value.Name == Drawer.GetClickedButton().Text)
-                    {
+            if (Drawer.DrawCapture) {
+                foreach (var m in ShopKeeper.Inventory.Captures) {
+                    if (m.Value.Name == Drawer.GetClickedButton().Text) {
                         SelectedCapture = m.Value;
                     }
                 }
             }
         }
-        public void Sell()
-        {
-            if (Drawer.DrawMedicine)
-            {
-                foreach (var m in ShopKeeper.Inventory.Medicine)
-                {
-                    if (m.Value.Name == Drawer.GetClickedButton().Text)
-                    {
+        public void Sell() {
+            if (Drawer.DrawMedicine) {
+                foreach (var m in ShopKeeper.Inventory.Medicine) {
+                    if (m.Value.Name == Drawer.GetClickedButton().Text) {
                         SelectedMedicine = m.Value;
                     }
                 }
             }
-            if (Drawer.DrawCapture)
-            {
-                foreach (var m in ShopKeeper.Inventory.Captures)
-                {
-                    if (m.Value.Name == Drawer.GetClickedButton().Text)
-                    {
+            if (Drawer.DrawCapture) {
+                foreach (var m in ShopKeeper.Inventory.Captures) {
+                    if (m.Value.Name == Drawer.GetClickedButton().Text) {
                         SelectedCapture = m.Value;
                     }
                 }
