c3f149e357b3be49cf913c8e925aebe553c56434
Frankbroeks <fbroeks@hetnet.nl>
2016-01-21 10:06:43 +0100

    Added conversations key. Added strings for the database

--- VideoGame/VideoGame/Forms/Settings.cs
+++ VideoGame/VideoGame/Forms/Settings.cs
@@ -17,6 +17,7 @@ namespace VideoGame.Forms
         public bool down;
         public bool right;
         public bool left;
+        public bool conversation;
 
         public Settings()
         {
@@ -30,7 +31,7 @@ namespace VideoGame.Forms
             if (up == true)
             { 
                 if (Classes.Settings.moveRight != (Keys) e.KeyCode && Classes.Settings.moveDown != (Keys) e.KeyCode &&
-                    Classes.Settings.moveLeft != (Keys) e.KeyCode)
+                    Classes.Settings.moveLeft != (Keys) e.KeyCode && Classes.Settings.conversation != (Keys)e.KeyCode)
                 {
                     Classes.Settings.moveUp = (Keys) e.KeyCode;
                     lblUp.Text = Convert.ToString(e.KeyCode);
@@ -44,7 +45,7 @@ namespace VideoGame.Forms
             if (down == true)
             {
                 if (Classes.Settings.moveRight != (Keys) e.KeyCode && Classes.Settings.moveUp != (Keys) e.KeyCode &&
-                    Classes.Settings.moveLeft != (Keys) e.KeyCode)
+                    Classes.Settings.moveLeft != (Keys) e.KeyCode && Classes.Settings.conversation != (Keys)e.KeyCode)
                 {
                     Classes.Settings.moveDown = (Keys) e.KeyCode;
                     lblDown.Text = Convert.ToString(e.KeyCode);
@@ -58,7 +59,7 @@ namespace VideoGame.Forms
             if (right == true)
             {
                 if (Classes.Settings.moveUp != (Keys) e.KeyCode && Classes.Settings.moveDown != (Keys) e.KeyCode &&
-                    Classes.Settings.moveLeft != (Keys) e.KeyCode)
+                    Classes.Settings.moveLeft != (Keys) e.KeyCode && Classes.Settings.conversation != (Keys)e.KeyCode)
                 {
                     Classes.Settings.moveRight = (Keys) e.KeyCode;
                     lblRight.Text = Convert.ToString(e.KeyCode);
@@ -72,11 +73,25 @@ namespace VideoGame.Forms
             if (left == true)
             {
                 if (Classes.Settings.moveRight != (Keys) e.KeyCode && Classes.Settings.moveDown != (Keys) e.KeyCode &&
-                    Classes.Settings.moveUp != (Keys) e.KeyCode)
+                    Classes.Settings.moveUp != (Keys) e.KeyCode && Classes.Settings.conversation != (Keys)e.KeyCode)
                 {
                     Classes.Settings.moveLeft = (Keys) e.KeyCode;
                     lblLeft.Text = Convert.ToString(e.KeyCode);
                     left = false;
+                }
+                else
+                {
+                    MessageBox.Show("This button was used before");
+                }
+            }
+            if (conversation == true)
+            {
+                if (Classes.Settings.moveRight != (Keys)e.KeyCode && Classes.Settings.moveDown != (Keys)e.KeyCode &&
+                    Classes.Settings.moveUp != (Keys)e.KeyCode && Classes.Settings.moveLeft != (Keys)e.KeyCode)
+                {
+                    Classes.Settings.conversation = (Keys)e.KeyCode;
+                    lblConversation.Text = Convert.ToString(e.KeyCode);
+                    conversation = false;
                 }
                 else
                 {
@@ -191,7 +206,16 @@ namespace VideoGame.Forms
 
         private void btnApply_Click(object sender, EventArgs e)
         {
+            Classes.Settings.ServerName = tbServerName.Text;
+            Classes.Settings.DatabaseName = tbDatabaseName.Text;
+            Classes.Settings.Username = tbUsername.Text;
+            Classes.Settings.Password = tbPassword.Text;
             this.Close();
+        }
+
+        private void btnConversation_Click(object sender, EventArgs e)
+        {
+            conversation = true;
         }
     }
 }
