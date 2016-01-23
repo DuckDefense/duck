bd2e1311186616a530f733e7b1f617f22ae62893
ruudschouten <zzding@live.nl>
2016-01-23 14:37:12 +0100

    Updated launcher to work with login

--- VideoGame/VideoGame/Forms/Launcher.cs
+++ VideoGame/VideoGame/Forms/Launcher.cs
@@ -6,52 +6,51 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Sandbox.Classes;
+using Sandbox.Forms;
 
-namespace VideoGame.Forms
-{
-    public partial class Launcher : Form
-    {
-        public bool isIngelogd;
-        public Launcher()
-        {
+namespace VideoGame.Forms {
+    public partial class Launcher : Form {
+        public static bool isIngelogd;
+        public Launcher() {
             InitializeComponent();
-            if (isIngelogd == true)
-            {
+            if (isIngelogd == true) {
                 btnPlay.Text = "Play";
             }
-            else
-            {
-                btnPlay.Text = "Play offline";
+            else {
+                btnPlay.Enabled = false;
+                btnSettings.Enabled = false;
+                //btnPlay.Text = "Play offline";
             }
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            if (isIngelogd == true)
-            {
+        private void btnLogin_Click(object sender, EventArgs e) {
+            using (var login = new Login())
+                login.ShowDialog();
+            btnPlay.Enabled = isIngelogd;
+            btnSettings.Enabled = isIngelogd;
+            if (isIngelogd == true) {
                 isIngelogd = false;
-                btnPlay.Text = "Play offline";
-                btnLogin.Text = "Login";
+                //btnPlay.Text = "Play offline";
+                //btnLogin.Text = "Login";
             }
-            else
-            {
+            else {
                 isIngelogd = true;
                 btnPlay.Text = "Play";
-                btnLogin.Text = "Logout";
+                //btnLogin.Text = "Logout";
             }
-            
+
         }
 
-        private void btnPlay_Click(object sender, EventArgs e)
-        {
+        private void btnPlay_Click(object sender, EventArgs e) {
             this.Hide();
+            DatabaseConnector.GetSettings(Login.PlayerId);
             using (var game = new Game1())
                 game.Run();
-            
+
         }
 
-        private void btnSettings_Click(object sender, EventArgs e)
-        {
+        private void btnSettings_Click(object sender, EventArgs e) {
             using (var setting = new Settings())
                 setting.ShowDialog();
         }
