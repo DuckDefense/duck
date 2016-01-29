3667b2a327b899752fe56681e2d127c6c662f238
ruudschouten <zzding@live.nl>
2016-01-29 14:01:31 +0100

    Box moving works, and drawing is updated aswel

--- VideoGame/VideoGame/Classes/Box.cs
+++ VideoGame/VideoGame/Classes/Box.cs
@@ -23,39 +23,44 @@ namespace Sandbox.Classes {
         private int Page;
         public int CurrentPage;
         private int maxMonstersWidth = 6;
-        private int maxMonstersHeight = 8;
+        private int maxMonstersHeight = 4;
         private int MaxMonstersPerPage => maxMonstersWidth * maxMonstersHeight;
 
         private Rectangle boxBounds;
-        public List<ContainerButton> Monsters;
-        public List<ContainerButton> Party;
+        private Character player;
+        public List<ContainerButton> Monsters = new List<ContainerButton>();
+        public List<ContainerButton> Party = new List<ContainerButton>();
         private List<Button> switchButtons;
-        private Monster selectedMonster;
-        private bool drawStats, drawSwitch;
+        private ContainerButton Selected;
+        private ContainerButton ToBeMoved;
+        private bool drawStats, drawSelected;
         public bool Visible;
 
         public void Access(Character player, Vector2 pos) {
-            Party = new List<ContainerButton>();
-            Monsters = new List<ContainerButton>();
-            CurrentPage = 1;
-            Page = 1;
-            var index = 0;
-            boxBounds = new Rectangle((int)pos.X, (int)pos.Y, 48 * maxMonstersWidth, 48 * maxMonstersHeight);
-            var rect = new Rectangle(-48, 0, 24, 24);
-            foreach (var mon in player.Box) {
-                if (player.Box.Count > MaxMonstersPerPage) {
-                    index++;
-                    if (index > MaxMonstersPerPage) {
-                        rect = new Rectangle(0, 0, 24, 24);
-                        Page++;
+            this.player = player;
+            if (player.Box.Count != Monsters.Count) {
+                Party = new List<ContainerButton>();
+                Monsters = new List<ContainerButton>();
+                CurrentPage = 1;
+                Page = 1;
+                var index = 0;
+                boxBounds = new Rectangle((int)pos.X, (int)pos.Y, 48 * maxMonstersWidth, 48 * maxMonstersHeight);
+                var rect = new Rectangle(-48, 0, 48, 48);
+                foreach (var mon in player.Box) {
+                    if (player.Box.Count > MaxMonstersPerPage) {
+                        index++;
+                        if (index > MaxMonstersPerPage) {
+                            rect = new Rectangle(0, 0, 48, 48);
+                            Page++;
+                        }
                     }
+                    rect = new Rectangle(rect.X += mon.PartySprite.Width, rect.Y, 48, 48);
+                    if (rect.X == 48 * (maxMonstersWidth)) {
+                        rect = new Rectangle(0, rect.Y += mon.PartySprite.Width, 48, 48);
+                    }
+                    var boxed = new BoxedMonster(mon, Page);
+                    Monsters.Add(new ContainerButton(new Button(rect, mon.PartySprite), boxed));
                 }
-                rect = new Rectangle(rect.X += mon.PartySprite.Width, rect.Y, 24, 24);
-                if (rect.X == 48 * (maxMonstersWidth)) {
-                    rect = new Rectangle(0, rect.Y += mon.PartySprite.Width, 24, 24);
-                }
-                var boxed = new BoxedMonster(mon, Page);
-                Monsters.Add(new ContainerButton(new Button(rect, mon.PartySprite), boxed));
             }
             GetParty(player);
             Visible = true;
@@ -67,16 +72,41 @@ namespace Sandbox.Classes {
 
         public void Update(MouseState cur, MouseState prev, GameTime gameTime) {
             if (Visible) {
-                foreach (var b in Monsters) {
+                for (int i = 0; i < Monsters.Count; i++) {
+                    var b = Monsters[i];
                     if (b.Button.IsClicked(cur, prev)) {
-                        selectedMonster = b.BoxedMonster.Monster;
+                        if (Selected != null) {
+                            if (Selected == b) Selected = null;
+                            else {
+                                ToBeMoved = b;
+                                if (Monsters.Contains(Selected)) { MoveMonster(false); }
+                                else { MoveMonsterToList(false); }
+                                Selected = null;
+                                ToBeMoved = null;
+                            }
+                        }
+                        else { Selected = b; }
                         drawStats = true;
-                        GetSwitch();
                     }
                     b.BoxedMonster.Monster.Update(gameTime);
                 }
-                foreach (var b in Party) {
-                    b.Monster.Update(gameTime);
+                for (int i = 0; i < Party.Count; i++) {
+                    var p = Party[i];
+                    if (p.Button.IsClicked(cur, prev)) {
+                        if (Selected != null) {
+                            if (Selected == p) Selected = null;
+                            else {
+                                ToBeMoved = p;
+                                if (Party.Contains(Selected)) { MoveMonster(true); }
+                                else { MoveMonsterToList(true); }
+                                Selected = null;
+                                ToBeMoved = null;
+                            }
+                        }
+                        else { Selected = p; }
+                        drawStats = true;
+                    }
+                    p.Monster.Update(gameTime);
                 }
             }
         }
@@ -88,39 +118,61 @@ namespace Sandbox.Classes {
                 foreach (var con in Monsters.Where(con => con.BoxedMonster.Page == CurrentPage)) {
                     //batch.Draw(con.BoxedMonster.Monster.PartySprite, con.Button.VectorPosition, Color.White);
 
-                    batch.Draw(con.BoxedMonster.Monster.PartySprite, con.Button.VectorPosition, null, con.BoxedMonster.Monster.SourceRectangle, null,0f, new Vector2(2,2), Color.White);
+                    batch.Draw(con.BoxedMonster.Monster.PartySprite, con.Button.VectorPosition, null, con.BoxedMonster.Monster.SourceRectangle, null, 0f, new Vector2(2, 2), Color.White);
                 }
+                DrawParty(batch);
 
                 if (drawStats) {
                     //Draw it beneath the box
-                    Drawer.DrawMonsterInfo(batch, selectedMonster,
-                        new Vector2(boxBounds.X, boxBounds.Height + ContentLoader.MonsterViewer.Height + 32));
+                    if (Selected != null)
+                        Drawer.DrawMonsterInfo(batch, Selected.BoxedMonster != null ? Selected.BoxedMonster.Monster : Selected.Monster, new Vector2(0, boxBounds.Height));
                 }
-                if (drawSwitch) {
-                    //Ask if the player would like to switch it out with any of the monsters in the party, or an empty spot
-                    DrawSwitch(batch);
-                }
-                DrawParty(batch);
             }
         }
 
-        private void GetSwitch() {
-            switchButtons = new List<Button>();
-            int index = 0;
-            var rect = new Rectangle(boxBounds.Width + 24, 24, 32, 32);
-            foreach (var mon in Party) {
-                index++;
-                switchButtons.Add(new Button(new Rectangle(rect.X, rect.Y += 24, rect.Height, rect.Width), ContentLoader.Button, ContentLoader.ButtonHover, ContentLoader.ButtonClicked,
-                    $"{index + 1}: {mon.Monster.Name}", ContentLoader.Arial));
+        private void MoveMonster(bool box) {
+            var selectedId = 0;
+            var toBeMovedId = 0;
+            if (!box) {
+                selectedId = Monsters.IndexOf(Selected);
+                toBeMovedId = Monsters.IndexOf(ToBeMoved);
+                player.Box.MoveItem(toBeMovedId, selectedId);
             }
+            else {
+                selectedId = Party.IndexOf(Selected);
+                toBeMovedId = Party.IndexOf(ToBeMoved);
+                player.Monsters.MoveItem(toBeMovedId, selectedId);
+            }
+            Monsters.Clear();
+            Party.Clear();
+            Access(player, new Vector2(0, 0));
         }
 
-        private void DrawSwitch(SpriteBatch batch) {
-            foreach (var btn in switchButtons) {
-                btn.Draw(batch);
+        private void MoveMonsterToList(bool box) {
+            int selectedId;
+            int toBeMovedId;
+            if (box) {
+                selectedId = Monsters.IndexOf(Selected);
+                toBeMovedId = Party.IndexOf(ToBeMoved);
+                player.Monsters.MoveToList(player.Box, selectedId, toBeMovedId);
             }
+            else {
+                selectedId = Party.IndexOf(Selected);
+                toBeMovedId = Monsters.IndexOf(ToBeMoved);
+                player.Monsters.MoveToList(player.Box, toBeMovedId, selectedId);
+            }
+            Monsters.Clear();
+            Party.Clear();
+            Access(player, new Vector2(0, 0));
         }
 
+        private void GetParty(Character player) {
+            var rect = new Rectangle(boxBounds.Width, -48, 48, 48);
+            foreach (var mon in player.Monsters) {
+                var button = new Button(new Rectangle(rect.X, rect.Y += 48, 48, 48), mon.PartySprite);
+                Party.Add(new ContainerButton(button, mon));
+            }
+        }
 
         private void DrawParty(SpriteBatch batch) {
             foreach (var con in Party) {
@@ -128,12 +180,5 @@ namespace Sandbox.Classes {
             }
         }
 
-        private void GetParty(Character player) {
-            var rect = new Rectangle(boxBounds.Width + 48, 24, 24, 24);
-            foreach (var mon in player.Monsters) {
-                var button = new Button(new Rectangle(rect.X, rect.Y += 24, 24, 24), mon.PartySprite);
-                Party.Add(new ContainerButton(button, mon));
-            }
-        }
     }
 }
