d11a92cac17f25afaabedb8f1666577dc21fb92a
ruudschouten <zzding@live.nl>
2016-02-01 09:56:37 +0100

    Updated so you can choose to register and choose a gender, defaults to male

--- VideoGame/VideoGame/Forms/Login.Designer.cs
+++ VideoGame/VideoGame/Forms/Login.Designer.cs
@@ -23,12 +23,26 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.btnLogin = new System.Windows.Forms.Button();
             this.tbPassword = new System.Windows.Forms.TextBox();
             this.lbPassword = new System.Windows.Forms.Label();
             this.tbUser = new System.Windows.Forms.TextBox();
             this.lbName = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
+            this.cbRegister = new System.Windows.Forms.CheckBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.lbGender = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(12, 96);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 22;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // tbPassword
             // 
@@ -36,53 +50,92 @@
             this.tbPassword.Name = "tbPassword";
             this.tbPassword.PasswordChar = '●';
             this.tbPassword.Size = new System.Drawing.Size(153, 20);
-            this.tbPassword.TabIndex = 11;
+            this.tbPassword.TabIndex = 21;
             // 
             // lbPassword
             // 
             this.lbPassword.AutoSize = true;
             this.lbPassword.Location = new System.Drawing.Point(12, 54);
             this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(108, 13);
-            this.lbPassword.TabIndex = 10;
-            this.lbPassword.Text = "Password (Deze ook)";
+            this.lbPassword.Size = new System.Drawing.Size(113, 13);
+            this.lbPassword.TabIndex = 20;
+            this.lbPassword.Text = "Password (Same here)";
             // 
             // tbUser
             // 
             this.tbUser.Location = new System.Drawing.Point(12, 26);
             this.tbUser.Name = "tbUser";
             this.tbUser.Size = new System.Drawing.Size(156, 20);
-            this.tbUser.TabIndex = 9;
+            this.tbUser.TabIndex = 19;
             // 
             // lbName
             // 
             this.lbName.AutoSize = true;
             this.lbName.Location = new System.Drawing.Point(12, 9);
             this.lbName.Name = "lbName";
-            this.lbName.Size = new System.Drawing.Size(139, 13);
-            this.lbName.TabIndex = 8;
-            this.lbName.Text = "Name (Hoofdletter gevoelig)";
+            this.lbName.Size = new System.Drawing.Size(112, 13);
+            this.lbName.TabIndex = 18;
+            this.lbName.Text = "Name (Case sensitive)";
             // 
-            // btnLogin
+            // cbRegister
             // 
-            this.btnLogin.Location = new System.Drawing.Point(12, 96);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
-            this.btnLogin.TabIndex = 12;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.cbRegister.AutoSize = true;
+            this.cbRegister.Location = new System.Drawing.Point(93, 100);
+            this.cbRegister.Name = "cbRegister";
+            this.cbRegister.Size = new System.Drawing.Size(65, 17);
+            this.cbRegister.TabIndex = 23;
+            this.cbRegister.Text = "Register";
+            this.cbRegister.UseVisualStyleBackColor = true;
+            this.cbRegister.CheckedChanged += new System.EventHandler(this.cbRegister_CheckedChanged);
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Checked = true;
+            this.rbMale.Location = new System.Drawing.Point(12, 138);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(48, 17);
+            this.rbMale.TabIndex = 24;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(66, 138);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbFemale.TabIndex = 25;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // lbGender
+            // 
+            this.lbGender.AutoSize = true;
+            this.lbGender.Location = new System.Drawing.Point(12, 122);
+            this.lbGender.Name = "lbGender";
+            this.lbGender.Size = new System.Drawing.Size(42, 13);
+            this.lbGender.TabIndex = 26;
+            this.lbGender.Text = "Gender";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(180, 131);
+            this.ClientSize = new System.Drawing.Size(224, 182);
+            this.Controls.Add(this.lbGender);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.cbRegister);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.tbUser);
             this.Controls.Add(this.lbName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -92,10 +145,14 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.TextBox tbPassword;
         private System.Windows.Forms.Label lbPassword;
         private System.Windows.Forms.TextBox tbUser;
         private System.Windows.Forms.Label lbName;
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox cbRegister;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.Label lbGender;
     }
 }
